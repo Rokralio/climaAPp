77f802c7edf62f1ef042323f93942a302d893a6f
Rokralio <rjgonzalez.1996@gmail.com>
2024-06-16 07:43:48 -0500

    estilo agregado con bootstrap

--- src/componentes/openweather/ApiClimaApp.jsx
+++ src/componentes/openweather/ApiClimaApp.jsx
@@ -22,7 +22,7 @@ export const ApiClimaApp = () => {
     } catch (error) {
       setClima('');
       console.error('Error al obtener la ciudad', error);
-      setError(`No se encontro el clima de "${city}"`)
+      setError(`No se encontró el clima de ${city}`);
 
     } finally{
       setCargando(false);
@@ -33,23 +33,22 @@ export const ApiClimaApp = () => {
     getClima(city);
   };
 
-  
   return (
-    <div>
+    <div className='border border-3 p-2'>
       <CityForm onFormSubmit={envForm}/>
-      {cargando && <p>Cargando...</p> }
-      {error && <p>{error}</p>}
-      {clima && (
-        <div>
-          <h2>Clima en {clima.name}</h2>
-          <p>Pais: {clima.sys.country}</p>
-          <p>Temperatura: {clima.main.temp} °C</p>
-          <p>Sensación térmica: {clima.main.feels_like}</p>
-          <p>Temperatura mínima: {clima.main.temp_min}</p>
-          <p>Temperatura máxima: {clima.main.temp_max}</p>
-        </div>
-      )}
-    </div>
+        {cargando && <p>Cargando...</p> }
+        {error && <p className="h5 rounded-1 text-center text-danger bg-light mt-3">{error}</p>}
+        {clima && (
+          <div className='border border-2 rounded-1 border-info px-3 mt-1'>
+            <h2 className='text-center'>Clima en {clima.name}</h2>
+            <p>Pais: {clima.sys.country}</p>
+            <p>Temperatura: {clima.main.temp} °C</p>
+            <p>Sensación térmica: {clima.main.feels_like}</p>
+            <p>Temperatura mínima: {clima.main.temp_min}</p>
+            <p>Temperatura máxima: {clima.main.temp_max}</p>
+          </div>
+        )}
+      </div>
   );
 };
 
--- src/componentes/ui/CityForm.jsx
+++ src/componentes/ui/CityForm.jsx
@@ -15,18 +15,26 @@ const [formSubmitted, setFormSubmitted] = useState(false);
     setFormSubmitted(true);
   }
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Ciudad:
-        <input 
-          type="text"
-          value={city}
-          onChange={handleCityChange}
-          placeholder='ciudad?'
-          required
-        />
-      </label>
-      <button type='submit'>Enviar</button>
-    </form>
+    <div className="border border-2 border-info p-3 rounded-1 bg-secondary">
+      <form onSubmit={handleSubmit}>
+        <label>
+          <h1>Bienvenido a ClimaApp</h1>
+          <span className="m-2">Quieres saber el clima de Ciudad:</span><br />
+          <div className="m-1 d-flex justify-content-center">
+          <input 
+            type="text"
+            value={city}
+            onChange={handleCityChange}
+            placeholder='ciudad?'
+            required
+            className="rounded "
+          />
+          </div>
+        </label><br />
+        <div className="m-1 d-flex justify-content-center">
+        <button type='submit' className="rounded">Enviar</button>
+        </div>
+      </form>
+    </div>
   );
 };
