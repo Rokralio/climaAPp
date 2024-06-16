561cafdebd5363857abb183e4f40353e1bd7df57
Rokralio <rjgonzalez.1996@gmail.com>
2024-06-16 02:50:46 -0500

    componente CityForm agregado

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { CityForm } from "./componentes/ui/CityForm";
 
 function App() {
   return (
     <>
       <h1>Bienvenido a ClimaApp</h1>
       <p>Quieres saber el clima de</p>
+      <CityForm/>
     </>
   );
 };
---
+++ src/componentes/ui/CityForm.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+export const CityForm = () => {
+const [city, setCity] = useState('');
+const [formSubmitted, setFormSubmitted] = useState(false);
+
+  const handleCityChange = (event) => {
+    setCity(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setFormSubmitted(true);
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Ciudad:
+        <input 
+          type="text"
+          value={city}
+          onChange={handleCityChange}
+          placeholder='ciudad?'
+          required
+        />
+      </label>
+      <button type='submit'>Enviar</button>
+    </form>
+  );
+};
