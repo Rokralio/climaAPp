ef7bf8be3d655a643a4a786ec86f97fb303f2cd5
Rokralio <rjgonzalez.1996@gmail.com>
2024-07-04 03:24:27 -0500

    stilos agregados fin 1ra parte

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <div className="container-fluid vh-100 d-flex flex-column justify-content-center align-items-center">
       <div className="col-12 col-md-6 d-flex flex-column align-items-center">
         <GetClimaFondo descripcionClima={descripcionClima} />
-        <div className="api-container  justify-content-center align-items-center">
+        <div className="api-container ">
           <ApiClimaApp className="appQueries" setDescripcionClima={setDescripcionClima} />
         </div>
       </div>
